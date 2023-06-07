770f7c69b2b6e0f2f914662add2a1e88e94dfd49
Rippah <dzoni.pantic@hotmail.com>
2023-06-07 18:10:11 +0200

    Test

--- chess_engine_mai_promising_moves.py
+++ chess_engine_mai_promising_moves.py
@@ -173,7 +173,7 @@ center_squares = {chess.E4, chess.E5, chess.D4, chess.D5}
 center_value = 75
 
 center_box = {chess.C3, chess.C4, chess.C5, chess.C6, chess.D3, chess.D6, chess.E3, chess.E6, chess.F3, chess.F4, chess.F5, chess.F6}
-center_box_value = 25
+center_box_value = 10
 
 def evaluate_board(BOARD):
     points = 0
@@ -305,15 +305,17 @@ def evaluate_position(BOARD):
     return total_evaluation
 
 
-
 def min_max_movement_depth_N_alphabeta_iterative(BOARD, N, alpha, beta):
     opening_move = reader.get(BOARD)
 
     if opening_move is not None:
-        return opening_move.move, alpha, beta, 0
-    
+        return opening_move.move, alpha, beta
+    if N == 0:
+        return random_movement(BOARD), alpha, beta
+
     best_move = None
     min_value = float('inf')
+    max_value = float('-inf')
 
     for move in BOARD.legal_moves:
         temp = deepcopy(BOARD)
@@ -325,38 +327,39 @@ def min_max_movement_depth_N_alphabeta_iterative(BOARD, N, alpha, beta):
         evaluate_score += evaluate_piece_safety(temp, BOARD.piece_at(move.from_square), move.from_square)
         evaluate_score += evaluate_piece_mobility(temp)
         evaluate_score += evaluate_position(temp)
-        
-        if not BOARD.turn:
-            if evaluate_score <= alpha:
-                return best_move, evaluate_score, beta, evaluate_score
+
+        if BOARD.turn:
+            if evaluate_score >= beta:
+                return best_move, alpha, beta
+
+            alpha = max(alpha, evaluate_score)
             
+            if evaluate_score > max_value:
+                max_value = evaluate_score
+                best_move = move
+        else:
+            if evaluate_score <= alpha:
+                return best_move, alpha, beta
+
             beta = min(beta, evaluate_score)
 
             if evaluate_score < min_value:
                 min_value = evaluate_score
                 best_move = move
-    
-    if N > 1:
-        _, _, _, evaluate_score = min_max_movement_depth_N_alphabeta_iterative(temp, N - 1, alpha, beta)
-        return best_move, alpha, beta, evaluate_score
-    else:
-        return best_move, alpha, beta, evaluate_score
-        
+
+
+    return best_move, alpha, beta
+
 def min_max_algorithm(BOARD):
-    max_depth = 10
     best_move = None
     alpha = float('-inf')
     beta = float('inf')
-    score = 0
 
-    for depth in range(1, max_depth + 1):
-        current_move, alpha, beta, evaluated_score = min_max_movement_depth_N_alphabeta_iterative(BOARD, depth, alpha, beta)
-        if best_move is None or evaluated_score < score:
+    for depth in range(1, 20):
+        current_move, alpha, beta = min_max_movement_depth_N_alphabeta_iterative(BOARD, depth, alpha, beta)
+        if current_move is not None:
             best_move = current_move
-            score = evaluated_score
 
-    print("Best move is: {} \tWith the evaluation score of: {}".format(best_move, evaluated_score))
     return best_move
-
 
 chess_display.chess_engine(chess_display.chess_board, min_max_algorithm, chess.BLACK)
